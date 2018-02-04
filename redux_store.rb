3a10cac4a82b5bbbca364ca4dd06ee0cf3f6b30a
Amrr Bakry <amrrbakry17@gmail.com>
2018-02-04 18:37:44 +0200

    Add combineReducers function

--- redux_store.rb
+++ redux_store.rb
@@ -9,6 +9,16 @@ module Redux
       dispatch({})
     end
 
+    def self.combine_reducers(reducers)
+      lambda do |state, action|
+        state ||= {}
+
+        reducers.each_with_object({}) do |(key, reducer), next_state|
+          next_state[key] = reducer.call(state[key], action)
+          next_state
+        end
+      end
+    end
 
     def dispatch(action)
       @state = @reducer.call(@state, action)
